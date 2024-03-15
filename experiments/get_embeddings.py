e8811bac7b4ef26d2a787430cba2ce0a0e7ad82d
Jennifer Melot <jtmelot@gmail.com>
2024-03-15 17:19:34 -0400

    Adapt code from dataflow documentation to run as local script

---
+++ experiments/get_embeddings.py
@@ -0,0 +1,63 @@
+"""
+Adapted from https://cloud.google.com/dataflow/docs/notebooks/huggingface_text_embeddings
+"""
+
+import argparse
+import tempfile
+import apache_beam as beam
+from apache_beam.ml.transforms.base import MLTransform
+from apache_beam.ml.transforms.embeddings.huggingface import SentenceTransformerEmbeddings
+
+
+content = [
+    {'x': 'How do I get a replacement Medicare card?'},
+    {'x': 'What is the monthly premium for Medicare Part B?'},
+    {'x': 'How do I terminate my Medicare Part B (medical insurance)?'},
+    {'x': 'How do I sign up for Medicare?'},
+    {'x': 'Can I sign up for Medicare Part B if I am working and have health insurance through an employer?'},
+    {'x': 'How do I sign up for Medicare Part B if I already have Part A?'},
+    {'x': 'What are Medicare late enrollment penalties?'},
+    {'x': 'What is Medicare and who can get it?'},
+    {'x': 'How can I get help with my Medicare Part A and Part B premiums?'},
+    {'x': 'What are the different parts of Medicare?'},
+    {'x': 'Will my Medicare premiums be higher because of my higher income?'},
+    {'x': 'What is TRICARE ?'},
+    {'x': "Should I sign up for Medicare Part B if I have Veterans' Benefits?"}
+]
+
+text_embedding_model_name = 'sentence-transformers/sentence-t5-large'
+
+
+# helper function that returns a dict containing only first
+# ten elements of generated embeddings
+def truncate_embeddings(d):
+  for key in d.keys():
+    d[key] = d[key][:10]
+  return d
+
+artifact_location_t5 = tempfile.mkdtemp(prefix='huggingface_')
+
+
+def run(model_name: str):
+    embedding_transform = SentenceTransformerEmbeddings(
+        model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2", columns=['x'])
+    with beam.Pipeline() as pipeline:
+        data_pcoll = (
+                pipeline
+                | "CreateData" >> beam.Create(content))
+        transformed_pcoll = (
+                data_pcoll
+                | "MLTransform" >> MLTransform(write_artifact_location=artifact_location_t5).with_transform(
+            embedding_transform))
+
+        transformed_pcoll | beam.Map(truncate_embeddings) | 'LogOutput' >> beam.Map(print)
+
+        transformed_pcoll | "PrintEmbeddingShape" >> beam.Map(lambda x: print(f"Embedding shape: {len(x['x'])}"))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", default="google-bert/bert-base-multilingual-cased")
+    args = parser.parse_args()
+
+    run(args.model)
