bc6d25a7a36cdebdc33578af02d16fc5e5d93063
Rebecca <rggelles@gmail.com>
2024-04-04 10:20:40 -0400

    Lint sql

--- sql/family_references.sql
+++ sql/family_references.sql
@@ -1,11 +1,11 @@
-SELECT
-  DISTINCT references.family_id,
+SELECT DISTINCT
+  references.family_id,
   links.family_id AS family_reference
 FROM
   unified_patents.references
 LEFT JOIN
   unified_patents.links
-ON
-  references.patent_reference = links.patent_id
+  ON
+    references.patent_reference = links.patent_id
 WHERE
   references.family_id != links.family_id
