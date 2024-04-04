aa549bfbf14481f547837a40beba6654d24efaa0
Rebecca <rggelles@gmail.com>
2024-04-04 10:13:48 -0400

    Fix repeated rows in all patents

---
+++ sql/family_references.sql
@@ -0,0 +1,11 @@
+SELECT
+  DISTINCT references.family_id,
+  links.family_id AS family_reference
+FROM
+  unified_patents.references
+LEFT JOIN
+  unified_patents.links
+ON
+  references.patent_reference = links.patent_id
+WHERE
+  references.family_id != links.family_id
