32d540d199b2dce69a7e5f174b477c534092c53f
Christian Schoeberl <cschoeberl51@gmail.com>
2024-04-18 09:43:48 -0400

    updating sql to include patent info

--- sql/LID_to_translate.sql
+++ sql/LID_to_translate.sql
@@ -10,9 +10,11 @@ For LID:
 */
 CREATE OR REPLACE TABLE staging_patent_clusters.LID_to_translate AS (
   WITH LID_patents AS (
-    # We need family ID's from the metadata table.
+    # We need family IDs from the metadata table.
+    # Get langs into array for later model check. 
     SELECT  
       family_id, 
+      ARRAY_AGG(md.language IGNORE NULLS) AS langs 
     FROM `staging_patent_clusters.patents_lid` lid
     JOIN `staging_patent_clusters.metadata_d_p_removed` md USING(patent_id)
     WHERE 
@@ -20,24 +22,27 @@ CREATE OR REPLACE TABLE staging_patent_clusters.LID_to_translate AS (
                   'eo', 'eu', 'fy', 'gn', 'ia', 'ie', 'la', 
                   'lb', 'ln', 'nn', 'no', 'oc', 'qu', 'sw',
                   'tk', 'tl', 'tt', 'vo', 'war')
+    GROUP BY family_id
   ), language_check_stage AS (
     # Ensured the same number of family IDs in this subquery as above. 
     SELECT 
       family_id, 
-      ARRAY_AGG(md.language IGNORE NULLS) AS langs, 
-      STRING_AGG(md.title_original) AS titles_original, 
-      STRING_AGG(md.abstract_original) AS abstracts_original
+      langs, 
+      patent_id, 
+      title_original, 
+      abstract_original
     FROM LID_patents
     JOIN `staging_patent_clusters.metadata_d_p_removed` md USING(family_id)
-    GROUP BY family_id
+    WHERE 
+      # Model language check. 
+      staging_patent_clusters.checkLanguages(langs) IS FALSE
+      # Just make sure we're not double-counting families/characters. 
+      AND family_id NOT IN (SELECT family_id FROM `staging_patent_clusters.UP_to_translate`)
   )
   SELECT 
     DISTINCT family_id, 
-    titles_original, 
-    abstracts_original 
+    patent_id, 
+    title_original, 
+    abstract_original
   FROM language_check_stage 
-  WHERE 
-    staging_patent_clusters.checkLanguages(langs) IS FALSE
-    # Just make sure we're not double-counting families/characters. 
-    AND family_id NOT IN (SELECT family_id FROM `staging_patent_clusters.UP_to_translate`)
 )
--- sql/UP_to_translate.sql
+++ sql/UP_to_translate.sql
@@ -7,27 +7,31 @@ For UP:
 */
 CREATE OR REPLACE TABLE staging_patent_clusters.UP_to_translate AS (
   WITH UP_patents AS (
+    # Get langs into array for later model check. 
     SELECT  
-      family_id
-    FROM `staging_patent_clusters.metadata_d_p_removed`
+      family_id, 
+      ARRAY_AGG(md.language IGNORE NULLS) AS langs
+    FROM `staging_patent_clusters.metadata_d_p_removed` md
     WHERE 
       language IN ('bs', 'is', 'no', 'ot', 'sh')
+    GROUP BY family_id
   ), language_check_stage AS (
     # Ensured the same number of family IDs in this subquery as above. 
     SELECT
       family_id, 
-      ARRAY_AGG(language IGNORE NULLS) AS langs, 
-      STRING_AGG(title_original) AS titles_original, 
-      STRING_AGG(abstract_original) AS abstracts_original
+      langs, 
+      patent_id, 
+      title_original, 
+      abstract_original
     FROM UP_patents
     JOIN `staging_patent_clusters.metadata_d_p_removed` USING(family_id)
-    GROUP BY family_id
+    # Model language check.
+    WHERE staging_patent_clusters.checkLanguages(langs) IS FALSE
   )
   SELECT 
     DISTINCT family_id, 
-    titles_original, 
-    abstracts_original 
+    patent_id, 
+    title_original, 
+    abstract_original
   FROM language_check_stage 
-  # Check for model coverage in a patent family's aggregated languages.
-  WHERE staging_patent_clusters.checkLanguages(langs) IS FALSE
 )
