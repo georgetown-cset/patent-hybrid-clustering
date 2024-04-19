e6367cf95f5d8eb713355a970b703ac2522c1003
Christian Schoeberl <cschoeberl51@gmail.com>
2024-04-19 09:21:01 -0400

    checked counts, back to the reduced set. hopefully right

--- sql/UP_to_translate.sql
+++ sql/UP_to_translate.sql
@@ -5,33 +5,33 @@ Goal:
 For UP: 
  - ['bs', 'is', 'no', 'ot', 'sh']
 */
-CREATE OR REPLACE TABLE staging_patent_clusters.UP_to_translate AS (
+CREATE OR REPLACE TABLE staging_patent_clusters.UP_to_translate_test AS (
   WITH UP_patents AS (
-    # Get langs into array for later model check. 
     SELECT  
-      family_id, 
-      ARRAY_AGG(md.language IGNORE NULLS) AS langs
-    FROM `staging_patent_clusters.metadata_d_p_removed` md
+      family_id
+    FROM `staging_patent_clusters.metadata_d_p_removed`
     WHERE 
       language IN ('bs', 'is', 'no', 'ot', 'sh')
-    GROUP BY family_id
-  ), language_check_stage AS (
+  ), model_check_stage AS (
     # Ensured the same number of family IDs in this subquery as above. 
     SELECT
       family_id, 
-      langs, 
-      patent_id, 
-      title_original, 
-      abstract_original
+      ARRAY_AGG(language IGNORE NULLS) AS langs
     FROM UP_patents
     JOIN `staging_patent_clusters.metadata_d_p_removed` USING(family_id)
-    # Model language check.
+    GROUP BY family_id
+  ), model_check AS (
+    SELECT 
+      DISTINCT family_id, 
+    FROM model_check_stage 
+    # Check for model coverage in a patent family's aggregated languages.
     WHERE staging_patent_clusters.checkLanguages(langs) IS FALSE
   )
   SELECT 
-    DISTINCT family_id, 
+    DISTINCT family_id,
     patent_id, 
     title_original, 
     abstract_original
-  FROM language_check_stage 
+  FROM model_check
+  JOIN `staging_patent_clusters.metadata_d_p_removed` USING(family_ID)
 )
