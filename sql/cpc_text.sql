b20bb16c5d0a1dd8ec40f723cfd3c0a6030a4bff
Christian Schoeberl <cschoeberl51@gmail.com>
2024-05-14 16:40:59 -0400

    adding skeleton of new CPC/IPC text linkage, it doesn't work

--- sql/cpc_text.sql
+++ sql/cpc_text.sql
@@ -1,9 +1,11 @@
 /*
 Goal: get CPC text where available, or substitute IPC matches when not available.
 Notes:
-  - ARRAY_CONCAT_AGG does not allow DISTINCT.
-  - Therefore, first UNNEST then STRING_AGG at the family level.
-  - Takes around 1 minute to run.
+  - CPC/IPCs have been updated:
+    - Both in Unified Patents, and in cpc_codes
+  - Table creation has not run
+    - The query is taking forever to run
+    - Something about the current approach is slow
 */
 CREATE OR REPLACE TABLE staging_patent_clusters.family_cpc_text AS (
   WITH stage AS (
@@ -19,25 +21,74 @@ CREATE OR REPLACE TABLE staging_patent_clusters.family_cpc_text AS (
   text_join AS (
     SELECT
       family_id,
-      CONCAT(COALESCE(TRIM(cpc_td.title), ''), " ", COALESCE(TRIM(cpc_td.description), '')) AS cpc_text,
-      CONCAT(COALESCE(TRIM(ipc_td.title), ''), " ", COALESCE(TRIM(ipc_td.description), '')) AS ipc_text
+      patent_id,
+      cpc_td.code AS cpc_code,
+      cpc_td.level AS cpc_level,
+      RANK() OVER(ORDER BY cpc_td.level) AS cpc_rank,
+      ipc_td.code AS ipc_code,
+      ipc_td.level AS ipc_level,
+      RANK() OVER(ORDER BY ipc_td.level) AS ipc_rank
     FROM stage
-    LEFT JOIN `cpc_codes.cpc_title_descriptions` AS cpc_td ON(cpcs = cpc_td.code)
-    LEFT JOIN `cpc_codes.cpc_title_descriptions` AS ipc_td ON(ipcs = ipc_td.code)
+    LEFT JOIN `cpc_codes.all_cpc_titles` AS cpc_td ON(cpcs = cpc_td.code)
+    LEFT JOIN `cpc_codes.all_cpc_titles` AS ipc_td ON(ipcs = ipc_td.code)
     WHERE (cpc_td.code IS NOT NULL OR ipc_td.code IS NOT NULL)
   ),
 
-  family_agg AS (
+  cpc_aggregation AS (
     SELECT
       family_id,
-      STRING_AGG(DISTINCT cpc_text, ". " ORDER BY cpc_text) AS cpc_text,
-      STRING_AGG(DISTINCT ipc_text, ". " ORDER BY ipc_text) AS ipc_text
+      patent_id,
+      cpc_code,
+      ARRAY_AGG(cpc_level ORDER BY cpc_rank DESC) AS cpc_array
     FROM text_join
+    GROUP BY family_id, patent_id, cpc_code
+  ),
+
+  ipc_aggregation AS (
+    SELECT
+      family_id,
+      patent_id,
+      ipc_code,
+      ARRAY_AGG(ipc_level ORDER BY ipc_rank DESC) AS ipc_array
+    FROM text_join
+    GROUP BY family_id, patent_id, ipc_code
+  ),
+
+  cpc_filtering AS (
+    SELECT
+      family_id,
+      patent_id,
+      cpc_code,
+      cpc_td.text AS cpc_text
+    FROM cpc_aggregation, UNNEST(cpc_array) cpc_level WITH OFFSET ranking
+    INNER JOIN `cpc_codes.all_cpc_titles` cpc_td ON(CONCAT(cpc_code, cpc_level) = CONCAT(cpc_td.code, cpc_td.level))
+    WHERE ranking = 0
+  ),
+
+  ipc_filtering AS (
+    SELECT
+      family_id,
+      patent_id,
+      ipc_td.text AS ipc_text
+    FROM ipc_aggregation, UNNEST(ipc_array) ipc_level WITH OFFSET ranking
+    INNER JOIN `cpc_codes.all_cpc_titles` ipc_td ON(CONCAT(ipc_code, ipc_level) = CONCAT(ipc_td.code, ipc_td.level))
+    WHERE ranking = 0
+  ),
+
+  family_stage AS (
+    SELECT
+      family_id,
+      STRING_AGG(DISTINCT cpc_text, ". ") AS cpc_text,
+      STRING_AGG(DISTINCT ipc_text, ". ") AS ipc_text
+    FROM ipc_filtering
+    FULL OUTER JOIN cpc_filtering USING(family_id)
     GROUP BY family_id
   )
 
   SELECT
     family_id,
-    COALESCE(cpc_text, ipc_text) AS text
-  FROM family_agg
+    COALESCE(cpc_text, ipc_text) AS cpc_text
+  FROM family_stage
+  ORDER BY family_id
+  LIMIT 20
 )
