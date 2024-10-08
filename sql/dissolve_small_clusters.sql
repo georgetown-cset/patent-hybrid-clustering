4096158d9b31775b53fab57f08121290288390e9
Katherine Quinn <kat.n.quinn@gmail.com>
2024-10-08 15:10:06 -0400

    add sql code to count number of links between patents from different clustering experiments
    fixing comment
    
    adding analysis notebook
    
    Update experiment_link_counting.sql
    
    fixing tables for counting
    
    fixing counting
    
    dissolving and reassigning small clusters
    
    adding assignment waves
    
    adding final cluster assignment sql
    
    fixing comments
    
    fixing stuff

---
+++ sql/dissolve_small_clusters.sql
@@ -0,0 +1,86 @@
+CREATE OR REPLACE TABLE staging_patent_clusters.cluster_assignment_small_reassigned AS (
+  WITH
+  all_clusters AS (
+    SELECT
+      *
+    FROM
+      staging_clustering_experiments.patent_cluster_pruning_experiment_sts_scaling_20241111_best_clusters_mapped_from_cset
+  ),
+
+  cluster_sizes AS (
+    SELECT
+      cluster_id,
+      COUNT(*) AS size
+    FROM all_clusters
+    GROUP BY cluster_id
+  ),
+
+  large_cluster_assignments AS (
+    SELECT
+      family_id,
+      cluster_id
+    FROM all_clusters
+    WHERE cluster_id IN (SELECT cluster_id FROM cluster_sizes WHERE size > 49)
+  ),
+
+  patent_cluster_weights AS (
+    SELECT DISTINCT
+      id AS family_id,
+      fam.cluster_id AS family_cluster,
+      ref_id,
+      ref.cluster_id AS ref_cluster,
+      weight
+    FROM staging_patent_clusters.hybrid_sts_scaled_weights
+    LEFT JOIN large_cluster_assignments AS fam ON id = fam.family_id
+    LEFT JOIN large_cluster_assignments AS ref ON ref_id = ref.family_id
+  ),
+
+  ref_weights AS (
+    SELECT
+      family_id,
+      ref_cluster AS cluster_id,
+      SUM(weight) AS ref_weight
+    FROM patent_cluster_weights
+    WHERE ref_cluster IS NOT NULL AND family_cluster IS NULL
+    GROUP BY family_id, ref_cluster
+  ),
+
+  cit_weights AS (
+    SELECT
+      ref_id AS family_id,
+      family_cluster AS cluster_id,
+      SUM(weight) AS cit_weight
+    FROM patent_cluster_weights
+    WHERE family_cluster IS NOT NULL AND ref_cluster IS NULL
+    GROUP BY ref_id, family_cluster
+  ),
+
+  total_weights AS (
+    SELECT
+      family_id,
+      cluster_id,
+      (COALESCE(ref_weight, 0.0) + COALESCE(cit_weight, 0.0)) / SQRT(size) AS scaled_total_weight
+    FROM ref_weights
+    FULL JOIN cit_weights USING (family_id, cluster_id)
+    FULL JOIN cluster_sizes USING (cluster_id)
+  ),
+
+  weight_rankings AS (
+    SELECT
+      family_id,
+      ARRAY_AGG(cluster_id IGNORE NULLS ORDER BY scaled_total_weight DESC)[0] AS cluster_id
+    FROM total_weights
+    GROUP BY family_id
+  )
+
+
+  SELECT
+    family_id,
+    cluster_id
+  FROM weight_rankings
+  UNION ALL
+  SELECT
+    family_id,
+    cluster_id
+  FROM large_cluster_assignments
+)
---
+++ sql/experiment_link_counting.sql
@@ -0,0 +1,97 @@
+/* Script to count the number of times edges in the patent hybrid network connect to patents in the
+same cluster for 5 experiments.
+
+Network: staging_patent_clusters.hybrid_sts_scaled_weights
+Experiments: staging_clustering_experiments.patent_cluster_intial_experiement_sts_voting_10012024_{X_clusters}
+             where X is the experiment number (0,1,2,3,4)
+
+Table saved in staging_patent_clusters
+*/
+
+CREATE OR REPLACE TABLE staging_patent_clusters.hybrid_sts_scaled_weights_link_counts AS (
+
+  WITH
+  -- Finding which patents are connected to each other via links
+  links AS (
+    SELECT DISTINCT
+      CAST(id AS STRING) AS node_id,
+      CAST(ref_id AS STRING) AS ref_id,
+      weight
+    FROM staging_patent_clusters.hybrid_sts_scaled_weights
+  ),
+
+  -- Finding the cluster assignments for each patent from each run
+  c0 AS (
+    SELECT
+      CAST(family_id AS STRING) AS node_id,
+      CAST(cluster_id AS STRING) AS cluster_id
+    FROM
+      staging_clustering_experiments.patent_cluster_intial_experiement_sts_voting_10012024_0_clusters_mapped_from_cset
+  ),
+
+  c1 AS (
+    SELECT
+      CAST(family_id AS STRING) AS node_id,
+      CAST(cluster_id AS STRING) AS cluster_id
+    FROM
+      staging_clustering_experiments.patent_cluster_intial_experiement_sts_voting_10012024_1_clusters_mapped_from_cset
+  ),
+
+  c2 AS (
+    SELECT
+      CAST(family_id AS STRING) AS node_id,
+      CAST(cluster_id AS STRING) AS cluster_id
+    FROM
+      staging_clustering_experiments.patent_cluster_intial_experiement_sts_voting_10012024_2_clusters_mapped_from_cset
+  ),
+
+  c3 AS (
+    SELECT
+      CAST(family_id AS STRING) AS node_id,
+      CAST(cluster_id AS STRING) AS cluster_id
+    FROM
+      staging_clustering_experiments.patent_cluster_intial_experiement_sts_voting_10012024_3_clusters_mapped_from_cset
+  ),
+
+  c4 AS (
+    SELECT
+      CAST(family_id AS STRING) AS node_id,
+      CAST(cluster_id AS STRING) AS cluster_id
+    FROM
+      staging_clustering_experiments.patent_cluster_intial_experiement_sts_voting_10012024_4_clusters_mapped_from_cset
+  ),
+
+  -- Seeing if each edge in the link graph connects to patents in the same cluster or not for each experiment
+  edge_matching AS (
+    SELECT
+      links.node_id AS node_id,
+      links.ref_id AS ref_id,
+      links.weight AS weight,
+      CASE WHEN c0a.cluster_id = c0b.cluster_id THEN 1 ELSE 0 END AS c0_match,
+      CASE WHEN c1a.cluster_id = c1b.cluster_id THEN 1 ELSE 0 END AS c1_match,
+      CASE WHEN c2a.cluster_id = c2b.cluster_id THEN 1 ELSE 0 END AS c2_match,
+      CASE WHEN c3a.cluster_id = c3b.cluster_id THEN 1 ELSE 0 END AS c3_match,
+      CASE WHEN c4a.cluster_id = c4b.cluster_id THEN 1 ELSE 0 END AS c4_match
+
+    FROM links
+    LEFT JOIN c0 AS c0a ON links.node_id = c0a.node_id
+    LEFT JOIN c0 AS c0b ON links.ref_id = c0b.node_id
+    LEFT JOIN c1 AS c1a ON links.node_id = c1a.node_id
+    LEFT JOIN c1 AS c1b ON links.ref_id = c1b.node_id
+    LEFT JOIN c2 AS c2a ON links.node_id = c2a.node_id
+    LEFT JOIN c2 AS c2b ON links.ref_id = c2b.node_id
+    LEFT JOIN c3 AS c3a ON links.node_id = c3a.node_id
+    LEFT JOIN c3 AS c3b ON links.ref_id = c3b.node_id
+    LEFT JOIN c4 AS c4a ON links.node_id = c4a.node_id
+    LEFT JOIN c4 AS c4b ON links.ref_id = c4b.node_id
+  )
+
+  -- Generating the edge list with number of times it connects to patents in the same cluster.
+  -- We will only keep edges that connect 2/5 times or more
+  SELECT
+    node_id,
+    ref_id,
+    weight,
+    c0_match + c1_match + c2_match + c3_match + c4_match AS link_count
+  FROM edge_matching
+)
