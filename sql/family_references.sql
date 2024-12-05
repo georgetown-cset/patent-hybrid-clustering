4869ad805a31c68b4ced2383e8f4aebdd31790ed
Rebecca <rggelles@gmail.com>
2024-12-05 15:18:26 -0500

    Fix dummy family issue in citations

--- sql/family_references.sql
+++ sql/family_references.sql
@@ -1,11 +1,28 @@
+WITH dummy_references AS (
+  SELECT DISTINCT
+    patent_id,
+    COALESCE(family_id, "X-" || patent_id) as family_id,
+    patent_reference
+  FROM
+    unified_patents.references
+),
+
+dummy_links AS (
+  SELECT DISTINCT
+    patent_id,
+    COALESCE(family_id, "X-" || patent_id) as family_id
+  FROM
+    unified_patents.links
+)
+
 SELECT DISTINCT
-  references.family_id,
+  dummy_references.family_id,
   links.family_id AS family_reference
 FROM
-  unified_patents.references
+  dummy_references
 LEFT JOIN
   unified_patents.links
   ON
-    references.patent_reference = links.patent_id
+    dummy_references.patent_reference = links.patent_id
 WHERE
-  references.family_id != links.family_id
+  dummy_references.family_id != links.family_id
