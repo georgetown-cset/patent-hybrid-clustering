24d6d5c97c796edde2db6192e7bee2f2a3faa5c3
Rebecca <rggelles@gmail.com>
2024-12-05 15:42:03 -0500

    And do another dummy family fix

--- sql/family_references.sql
+++ sql/family_references.sql
@@ -1,7 +1,7 @@
 WITH dummy_references AS (
   SELECT DISTINCT
     patent_id,
-    COALESCE(family_id, "X-" || patent_id) as family_id,
+    COALESCE(family_id, "X-" || patent_id) AS family_id,
     patent_reference
   FROM
     unified_patents.references
@@ -10,19 +10,19 @@ WITH dummy_references AS (
 dummy_links AS (
   SELECT DISTINCT
     patent_id,
-    COALESCE(family_id, "X-" || patent_id) as family_id
+    COALESCE(family_id, "X-" || patent_id) AS family_id
   FROM
     unified_patents.links
 )
 
 SELECT DISTINCT
   dummy_references.family_id,
-  links.family_id AS family_reference
+  dummy_links.family_id AS family_reference
 FROM
   dummy_references
 LEFT JOIN
-  unified_patents.links
+  dummy_links
   ON
-    dummy_references.patent_reference = links.patent_id
+    dummy_references.patent_reference = dummy_links.patent_id
 WHERE
-  dummy_references.family_id != links.family_id
+  dummy_references.family_id != dummy_links.family_id
