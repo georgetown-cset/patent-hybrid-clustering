d857ba21a15e089ff9905564740309fc158317e0
Rebecca <rggelles@gmail.com>
2025-01-09 15:09:12 -0500

    Add push_to_airflow.sh script

---
+++ push_to_airflow.sh
@@ -0,0 +1,11 @@
+gsutil cp patent_hybrid_clustering_dag.py gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/
+gsutil rm gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/sql/patent_clustering/*
+
+gsutil cp sql/*.sql gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/sql/patent_clustering/
+gsutil cp sql/breakdowns/* gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/sql/patent_clustering/
+gsutil cp sql/checks/* gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/sql/patent_clustering/
+
+gsutil cp schemas/* gs://airflow-data-exchange/patent_clustering/schemas/
+gsutil cp schemas/* gs://us-east1-production-cc2-202-b42a7a54-bucket/dags/schemas/patent_clustering/
+gsutil cp scripts/* gs://airflow-data-exchange/patent_clustering/model/
+gsutil cp sequences/* gs://airflow-data-exchange/patent_clustering/sequences/
