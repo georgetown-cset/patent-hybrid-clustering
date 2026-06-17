346f7c8761ac141371a24ac652e6797aef77357a
Shruti <sa2330@georgetown.edu>
2026-06-17 11:56:49 -0400

    added push to airflow to gh workflow

--- push_to_airflow.sh
+++ push_to_airflow.sh
@@ -1,19 +1,19 @@
 DAGS_FOLDER_GCS="us-east1-production-cc2-202-b42a7a54-bucket/dags"
 
-gsutil cp patent_hybrid_clustering_dag.py gs://$DAGS_FOLDER_GCS/
-#gsutil cp faiss_dag.py gs://$DAGS_FOLDER_GCS/
-gsutil rm gs://$DAGS_FOLDER_GCS/sql/patent_clusters/*
+gcloud storage cp patent_hybrid_clustering_dag.py gs://$DAGS_FOLDER_GCS/
+#gcloud storage cp faiss_dag.py gs://$DAGS_FOLDER_GCS/
+gcloud storage rm gs://$DAGS_FOLDER_GCS/sql/patent_clusters/*
 
-gsutil cp sql/*.sql gs://$DAGS_FOLDER_GCS/sql/patent_clusters/
-gsutil cp sql/breakdowns/* gs://$DAGS_FOLDER_GCS/sql/patent_clusters/
-gsutil cp sql/checks/* gs://$DAGS_FOLDER_GCS/sql/patent_clusters/
+gcloud storage cp sql/*.sql gs://$DAGS_FOLDER_GCS/sql/patent_clusters/
+gcloud storage cp sql/breakdowns/* gs://$DAGS_FOLDER_GCS/sql/patent_clusters/
+gcloud storage cp sql/checks/* gs://$DAGS_FOLDER_GCS/sql/patent_clusters/
 
-gsutil cp schemas/* gs://airflow-data-exchange/patent_clusters/schemas/
-gsutil cp schemas/* gs://$DAGS_FOLDER_GCS/patent_clusters/schemas/
-gsutil cp scripts/* gs://airflow-data-exchange/patent_clusters/model/
-gsutil cp scripts/similarity.py gs://airflow-data-exchange/patent_clusters/scripts/
-gsutil cp sequences/* gs://$DAGS_FOLDER_GCS/sequences/patent_clusters/
-gsutil cp scripts/get_embeddings* gs://$DAGS_FOLDER_GCS/patent_clusters/
+gcloud storage cp schemas/* gs://airflow-data-exchange/patent_clusters/schemas/
+gcloud storage cp schemas/* gs://$DAGS_FOLDER_GCS/patent_clusters/schemas/
+gcloud storage cp scripts/* gs://airflow-data-exchange/patent_clusters/model/
+gcloud storage cp scripts/similarity.py gs://airflow-data-exchange/patent_clusters/scripts/
+gcloud storage cp sequences/* gs://$DAGS_FOLDER_GCS/sequences/patent_clusters/
+gcloud storage cp scripts/get_embeddings* gs://$DAGS_FOLDER_GCS/patent_clusters/
 
 # mop_bq_to_bq_metadata DAG: uses the template DAG in airflow_sql_sequences_templates (sql_updater_with_checks_dag.py)
 gcloud storage cp config/mop_bq_to_bq_metadata.json gs://$DAGS_FOLDER_GCS/sql_updater_with_checks_config/
